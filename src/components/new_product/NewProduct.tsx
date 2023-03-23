c299db0491f823be85f50c081f35198ae21dd411
Dmytrii9Cherniak <dcheyrnak10@gmail.com>
2023-03-23 22:55:25 +0200

    added form and validation

--- src/components/new_product/NewProduct.tsx
+++ src/components/new_product/NewProduct.tsx
@@ -1,24 +1,61 @@
 import React from 'react';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 import './NewProduct.scss'
 
 function NewProduct() {
 
-    return (<div className="newProductPage">
-        <form>
-            <label htmlFor="name"> Name </label>
-            <input id="name" type="text" />
 
-            <label htmlFor="author"> Author </label>
-            <input id="author" type="text" />
+    const validationSchema = Yup.object().shape({
+        name: Yup.string().required('Name is required'),
+        author: Yup.string().required('Author is required'),
+        year: Yup.string().required('Year of publication is required'),
+        rating: Yup.string()
+            .min(0, 'Rating must be greater than or equal to 0')
+            .max(5, 'Rating must be less than or equal to 5')
+            .required('Rating is required'),
+    });
 
-            <label htmlFor="year"> Year of publication </label>
-            <input id="year" type="date" />
+    return (
+        <div className="newProductPage">
+            <Formik
+                initialValues={{ name: '', author: '', year: '', rating: '' }}
+                validationSchema={validationSchema}
+                onSubmit={(values) => {
+                    console.log(values);
+                }}>
 
-            <label htmlFor="rating"> Rating </label>
-            <input id="rating" type="number" />
-            <button> Create </button>
-        </form>
-            </div>);
+                {({ errors, touched, dirty }) => (
+                    <Form>
+                        <label htmlFor="name"> Name </label>
+                        <Field id="name" name="name" type="text" />
+                        {errors.name && touched.name && !dirty ? (
+                            <div className="error">{errors.name}</div>
+                        ) : null}
+
+                        <label htmlFor="author"> Author </label>
+                        <Field id="author" name="author" type="text" />
+                        {errors.author && touched.author && !dirty ? (
+                            <div className="error">{errors.author}</div>
+                        ) : null}
+
+                        <label htmlFor="year"> Year of publication </label>
+                        <Field id="year" name="year" type="number" />
+                        {errors.year && touched.year && !dirty ? (
+                            <div className="error">{errors.year}</div>
+                        ) : null}
+
+                        <label htmlFor="rating"> Rating </label>
+                        <Field id="rating" name="rating" type="number" />
+                        {errors.rating && touched.rating && !dirty ? (
+                            <div className="error">{errors.rating}</div>
+                        ) : null}
+                        <button> Create </button>
+                    </Form>
+                )}
+            </Formik>
+        </div>
+    );
 }
 
 export default NewProduct;
