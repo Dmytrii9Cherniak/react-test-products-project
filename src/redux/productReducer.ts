948fe5be9e19b17d8f9d1a566fc11af48b83ff1a
Dmytrii9Cherniak <dcheyrnak10@gmail.com>
2023-03-24 10:17:03 +0200

    added update and delete to reducers

--- src/redux/productReducer.ts
+++ src/redux/productReducer.ts
@@ -24,6 +24,19 @@ export const ProductReducer = (state = initialState, action: ProductActionModel)
             return { loading: false, error: action.payload, products: [...state.products] };
         case Action_types.CREATE_NEW_PRODUCT_SUCCESS:
             return { ...state, loading: false, products: [...state.products, action.payload] };
+        case Action_types.UPDATE_EXISTS_PRODUCT:
+            return { ...state, loading: true };
+        case Action_types.UPDATE_EXISTS_PRODUCT_ERROR:
+            return { ...state, loading: false, error: action.payload };
+        case Action_types.UPDATE_EXISTS_PRODUCT_SUCCESS:
+            const updatedProductIndex = state.products.findIndex(product => product.id === action.payload.id);
+            const updatedProducts = [...state.products];
+            updatedProducts[updatedProductIndex] = action.payload;
+            return { ...state, loading: false, products: updatedProducts };
+        case Action_types.DELETE_EXISTS_PRODUCT:
+            const deletedProductId = action.payload;
+            const remainingProducts = state.products.filter(product => product.id !== deletedProductId);
+            return { ...state, loading: false, products: remainingProducts };
         default:
             return state;
     }
